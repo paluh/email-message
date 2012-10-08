a63bf1a0472534c4cedec28190c5dcc5d48a009b
Tomasz Rybarczyk <paluho@gmail.com>
2012-10-08 16:14:42 +0200

    Typo

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='email_message',
+    name='email-message',
     author='Tomasz Rybarczyk',
     author_email='paluho@gmail.com',
     version = '2012.9.1',
-    url='http://github.com/paluh/email_message',
+    url='http://github.com/paluh/email-message',
     py_modules=['email_message'],
     description='Email message wrapper ripped from django.',
     zip_safe=False,
